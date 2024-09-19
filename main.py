9e7ee267e1af08947124222222eade99af0b2b5b
Huab123 <89098516+Huab123@users.noreply.github.com>
2024-09-19 12:05:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import pyautogui
 import time
 
 
-def autoTrain():
+def autoTrain(typeTrain):
     webbrowser.open('https://www.torn.com/gym.php', new=0, autoraise=True)
     time.sleep(5)
-    pyautogui.leftClick(1275, 475, duration=0.1)
-    time.sleep(1)
+    pyautogui.leftClick(typeTrain[0], typeTrain[1], duration=0.1)  # Clicks chosen Train
+    time.sleep(1)                                                  # You should set single tap train to use 100 Energy
     pyautogui.hotkey('ctrl', 'w')
 
 
@@ -17,44 +17,71 @@ def autoCrime():
     time.sleep(5)
     while done < 3:
         if done == 0:
-            pyautogui.leftClick(1250, 675, duration=0.1)
+            pyautogui.leftClick(1250, 675, duration=0.1)  # Clicks to Armed Robberies
             time.sleep(2)
-            pyautogui.leftClick(1400, 750, duration=0.1)
+            pyautogui.leftClick(1400, 750, duration=0.1)  # Clicks to Thorough Armored Car
         elif done < 3:
             time.sleep(3)
-            pyautogui.leftClick(675, 475, duration=0.1)
+            pyautogui.leftClick(675, 475, duration=0.1)  # Clicks Try Again
         done += 1
     time.sleep(1)
     pyautogui.hotkey('ctrl', 'w')
 
 
+def typeGym():
+    strength = [775, 475]
+    defense = [1025, 475]
+    speed = [1275, 475]
+    dexterity = [1515, 475]
+    while True:
+        try:
+            typeTrain = int(input("Type of Training Strength(1), Defense(2), Speed(3), Dexterity(4): "))
+            if 4 >= typeTrain > 0:
+                break
+            else:
+                print("Must enter value 1-4")
+        except ValueError:
+            print("Must enter value 1-4")
+    if typeTrain == 1:
+        return strength
+    elif typeTrain == 2:
+        return defense
+    elif typeTrain == 3:
+        return speed
+    elif typeTrain == 4:
+        return dexterity
+
+
 def nerveTimer():
     while True:
         try:
-            nerveAmt = int(input("Nerve: "))
-            break
+            nerveAmt = int(input("Nerve: "))  # User input amount of Nerve
+            if nerveAmt > 0:
+                break
+            else:
+                print("You must enter a number greater than 0")
         except ValueError:
             print("You must enter a valid number")
     return nerveAmt * 5
 
 
 def runScript():
+    typeTrain = typeGym()
     nerveTime = nerveTimer()
     minute = 0
     energyTime = 0
     while True:
         print(minute)
-        if energyTime % 300 == 0:
-            autoTrain()
+        if energyTime % 300 == 0:  # Every 5 Hours auto trains
+            autoTrain(typeTrain)
             energyTime = 0
-        if minute % nerveTime == 0:
+        if minute % nerveTime == 0:  # Every (Nerve amount * 5) minutes auto crimes
             autoCrime()
             minute = 0
         minute += 5
         energyTime += 5
-        time.sleep(300)
+        time.sleep(300)  # Iterates every 5 minutes
 
 
 if __name__ == '__main__':
     runScript()
-
